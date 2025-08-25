6cbd781c34135ce06ccc42a1686eae76b6ca29ed
Gokamole <gokamole@tutamail.com>
2025-08-25 11:16:45 +0300

    4

--- 2/Player.py
+++ 2/Player.py
@@ -6,36 +6,26 @@ class Player:
 
     def gain_experience(self):
         self.experience = self.experience + int(input("Кол-во опыта: "))
-        if self.level == 0:
-            if self.experience >= 100:
-                self.level =+ 1
-        if self.level == 1:
-            if self.experience >= 200:
-                self.level =+ 1
-        if self.level == 2:
-            if self.experience >= 400:
-                self.level =+ 1
-        if self.level == 3:
-            if self.experience >= 800:
-                self.level =+ 1
-        if self.level == 4:
-            if self.experience >= 1600:
-                self.level =+ 1
-        if self.level == 5:
-            if self.experience >= 3200:
-                self.level =+ 1
-        if self.level == 6:
-            if self.experience >= 6400:
-                self.level =+ 1
-        if self.level == 7:
-            if self.experience >= 12800:
-                self.level =+ 1
-        if self.level == 8:
-            if self.experience >= 25600:
-                self.level =+ 1
-        if self.level == 9:
-            if self.experience >= 51200:
-                self.level =+ 1
+        if self.experience >=100:
+            self.level = 1
+        if self.experience >=200:
+            self.level = 2
+        if self.experience >=400:
+            self.level = 3
+        if self.experience >=800:
+            self.level = 4
+        if self.experience >=1600:
+            self.level = 5
+        if self.experience >=3200:
+            self.level = 6
+        if self.experience >=6400:
+            self.level = 7
+        if self.experience >=12800:
+            self.level = 8
+        if self.experience >=25600:
+            self.level = 9
+        if self.experience >=51200:
+            self.level = 10
     
     def show_stats(self):
         print(self.name)
