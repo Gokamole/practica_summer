e459d0e7a587eabb7c7f0b2831127742842385d2
Gokamole <gokamole@tutamail.com>
2025-08-25 11:25:45 +0300

    6

--- 2/main.py
+++ 2/main.py
@@ -1,5 +1,11 @@
 from Player import Player
+from Player import Warrior
 
 player = Player("Плэер", 0, 0)
 player.gain_experience()
 player.show_stats()
+
+warrior = Warrior("Вариор", 0, 0, 50, 50)
+warrior.attack()
+warrior.defend()
+warrior.show_info()
