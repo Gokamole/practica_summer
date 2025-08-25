c63b67ad13d0f3bbae652740b431d7280aff5fca
Gokamole <gokamole@tutamail.com>
2025-08-25 11:15:04 +0300

    3

--- 2/Player.py
+++ 2/Player.py
@@ -5,9 +5,37 @@ class Player:
         self.experience = experience
 
     def gain_experience(self):
-        self.experience = self.experience + int(input("Кол-во опыта"))
-        if self.experience >= 100:
-            self.level =+ 1
+        self.experience = self.experience + int(input("Кол-во опыта: "))
+        if self.level == 0:
+            if self.experience >= 100:
+                self.level =+ 1
+        if self.level == 1:
+            if self.experience >= 200:
+                self.level =+ 1
+        if self.level == 2:
+            if self.experience >= 400:
+                self.level =+ 1
+        if self.level == 3:
+            if self.experience >= 800:
+                self.level =+ 1
+        if self.level == 4:
+            if self.experience >= 1600:
+                self.level =+ 1
+        if self.level == 5:
+            if self.experience >= 3200:
+                self.level =+ 1
+        if self.level == 6:
+            if self.experience >= 6400:
+                self.level =+ 1
+        if self.level == 7:
+            if self.experience >= 12800:
+                self.level =+ 1
+        if self.level == 8:
+            if self.experience >= 25600:
+                self.level =+ 1
+        if self.level == 9:
+            if self.experience >= 51200:
+                self.level =+ 1
     
     def show_stats(self):
         print(self.name)
